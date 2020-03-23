90d4933d85fcfbf45bea83747ccd22acf31771bb
volodymyrsliusarenko <volodymyr.slusarenko@gmail.com>
2020-03-23 14:47:27 +0000

    add clock and form

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
 new ProductList(new Cart());
+
+const clock = document.querySelector('.clock');
+const showClock = () => clock.innerText = (new Date()).toLocaleTimeString();
+setInterval(showClock, 1000);
